ee4abd55b54728aaf1b111d0bf4b5f2b7c05ee20
JIang Peng <78525672+cooldaya@users.noreply.github.com>
2025-02-08 11:40:34 +0800

    添加demo

---
+++ 3.pdf-view/readme.md
@@ -0,0 +1,2 @@
+预览pdf，测试链接：
+    http://127.0.0.1:5500/3.pdf-view/index.html?file=./test.pdf
---
+++ demo-list.js
@@ -0,0 +1,22 @@
+const demos = [
+    {
+        name: "web-dwg",
+        description: "web中预览dwg文件",
+        dir: '1.web-dwg',
+        url: './1.web-dwg/index.html'
+    },
+    {
+        name: "reconnet-websockt",
+        description: "websockt链接，带重连功能",
+        dir: '2.reconnet-websockt',
+        url: './2.reconnet-websockt/index.html'
+    },
+    {
+        name: "pdf-view",
+        description: "pdf快捷预览，自定义预览效果",
+        dir: '3.pdf-view',
+        url: './3.pdf-view/index.html'
+    },
+    // 可以在这里添加更多的demo对象
+];
+window.demos = demos;
